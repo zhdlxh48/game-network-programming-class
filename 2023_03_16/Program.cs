5e7b9fd45356359db624f70aa80616df9a2740be
zhdlxh48 <zhdlxh48@gmail.com>
2023-03-16 11:01:45 +0900

    [2023-03-16] Add IpAddress variables

--- 2023_03_16/Program.cs
+++ 2023_03_16/Program.cs
@@ -14,11 +14,25 @@ namespace _2023_03_16
         {
             Socket socket = null;
 
+            IPAddress ipAddrLocal = null;
+            IPAddress ipAddrRemote = null;
+            IPAddress ipAddrLoopback = null;
+
             // Create socket with catch exception
             try
             {
+                ipAddrLocal = IPAddress.Parse("192.168.30.236");
+                ipAddrRemote = IPAddress.Parse("192.168.30.46");
+                ipAddrLoopback = IPAddress.Loopback;
+                Console.WriteLine("Local: {0}\nRemote: {1}\nLoopback: {2}", ipAddrLocal, ipAddrRemote, ipAddrLoopback);
+
                 socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 Console.WriteLine("Successfully creation socket");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("FormatException occoured: " + ex.Message);
+                Environment.Exit(-1);
             }
             catch (SocketException ex)
             {
