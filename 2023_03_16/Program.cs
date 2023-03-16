7b0739464317c0699397a419e9a024cc51434533
zhdlxh48 <zhdlxh48@gmail.com>
2023-03-16 10:40:37 +0900

    [2023-03-16] Add simple socket project

---
+++ 2023_03_16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace _2023_03_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = null;
+
+            // Create socket with catch exception
+            try
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Console.WriteLine("Successfully creation socket");
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("SocketException occoured: (" + ex.ErrorCode + ") " + ex.Message);
+                Environment.Exit(ex.ErrorCode);
+            }
+
+            //Close socket
+            // TODO: What diffrence between Close() and Shutdown()
+            socket.Close();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# game-network-programming-class
+# 게임네트워크프로그래밍 실습
 청강문화산업대학교 게임네트워크프로그래밍 수업 실습 리포지토리
+
+## 변경사항
+- 2023-03-16: 소켓 생성
+
+### SocketType
+- SocketType.Stream: TCP
+- SocketType.Dgram: UDP
+- SocketType.Raw: Editable socket type
