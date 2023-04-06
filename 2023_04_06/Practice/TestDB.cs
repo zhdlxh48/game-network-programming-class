258814772132b41fdfb0042d41b281479268714c
zhdlxh48 <zhdlxh48@gmail.com>
2023-04-06 11:43:12 +0900

    Add 2023_04_06, DB Conn example

---
+++ 2023_04_06/Practice/TestDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Practice
+{
+    public class TestDB
+    {
+        public static void Run()
+        {
+            var connStr = "Server=localhost;Database=testdb;Uid=root;Pwd=root";
+            var conn = new MySqlConnection(connStr);
+
+            try
+            {
+                conn.Open();
+
+                var sqlStr = "INSERT INTO test VALUES (10, 'IronMan');";
+                //var sqlStr = "DELETE FROM test WHERE Name = 'WonderWoman'";
+                var cmd = new MySqlCommand(sqlStr, conn);
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                throw;
+            }
+        }
+    }
+}
---
+++ 2023_04_06/Program.cs
@@ -0,0 +1,17 @@
+﻿using Practice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_04_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDB.Run();
+        }
+    }
+}
