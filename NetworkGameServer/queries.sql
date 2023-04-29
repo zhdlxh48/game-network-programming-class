b46241acbd43d8501d705f74a91e19b59c4c0b23
zhdlxh48 <zhdlxh48@gmail.com>
2023-04-30 06:32:03 +0900

    Update queries.sql

--- NetworkGameServer/queries.sql
+++ NetworkGameServer/queries.sql
@@ -4,3 +4,15 @@ grant all privileges on *.* to 'test'@'localhost';
 
 create database ckgame;
 
+use ckgame;
+
+create table scores (
+	distance float default 0.0, 
+    time float default 0.0
+);
+
+insert into scores (distance, time) values (10.3451, 5.1425);
+
+select * from scores;
+
+select * from scores order by distance asc LIMIT 4;
