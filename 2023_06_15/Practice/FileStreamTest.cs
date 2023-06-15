2c1bc5fc85f0d33a0a34d48e86bb6df21ac10af5
zhdlxh48 <zhdlxh48@gmail.com>
2023-06-15 11:51:28 +0900

    2023-06-15

---
+++ 2023_06_15/Practice/FileStreamTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class FileStreamTest
+    {
+        public static void Run() 
+        {
+            var path = @"C:\windows\system32\drivers\etc\HOSTS";
+
+            ReadAwaitable(path);
+
+            Console.ReadLine();
+        }
+
+        private static async void ReadAwaitable(string path)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                var buffer = new byte[fileStream.Length];
+                // fileStream.Read(buffer, 0, buffer.Length);
+                await fileStream.ReadAsync(buffer, 0, buffer.Length);
+
+                var text = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
---
+++ 2023_06_15/Program.cs
@@ -0,0 +1,17 @@
+﻿using Practice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_06_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStreamTest.Run();
+        }
+    }
+}
